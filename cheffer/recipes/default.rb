77025e06adf4d93b28258e686ca7c5edb91ee023
Jonathan Weiss <jw@innerewut.de>
2011-03-16 10:46:14 +0100

    add test recipe

---
+++ cheffer/recipes/default.rb
@@ -0,0 +1,7 @@
+gem_packge "right_aws"
+require "right_aws"
+Chef::Log.info "XXX: AWS: #{defined(RightAws).inspect}"
+
+gem_package "rocking_chair"
+require "rocking_chair"
+Chef::Log.info "XXX: RockingChair: #{defined(RockingChair).inspect}"
