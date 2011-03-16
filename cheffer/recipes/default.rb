343b61973e8cd7b7e7e300c3ce797b959bc58b13
Jonathan Weiss <jw@innerewut.de>
2011-03-16 12:23:18 +0100

    fix version call

--- cheffer/recipes/default.rb
+++ cheffer/recipes/default.rb
@@ -2,7 +2,10 @@ gem_package "right_aws"
 require "right_aws"
 Chef::Log.info "XXX: AWS: #{defined?(RightAws).inspect}"
 
-gem_package "activesupport", '3.0.5'
+gem_package "activesupport" do
+  version '3.0.5'
+end
+
 gem_package "rocking_chair"
 ruby_block 'test rocking chair' do
   block do
